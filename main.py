b1e2fc9981b5da33226e1ee6afc507d808ca8450
wpted <silverhandsome@gmail.com>
2022-04-24 23:46:21 +0800

    Initial try of transfering text to sound. Import playsound and gTTs.

---
+++ main.py
@@ -0,0 +1,11 @@
+from gtts import gTTS
+from playsound import playsound
+
+my_text = "Hello World"
+
+language = "en"
+
+my_obj = gTTS(text=my_text, lang=language, slow=False)
+my_obj.save("hello_world.mp3")
+
+playsound("hello_world.mp3")
